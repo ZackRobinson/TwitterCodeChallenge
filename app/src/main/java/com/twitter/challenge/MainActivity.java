07c4e5418594fca725bc3fc4bdebf6a88acc1a4b
zrobinson <zack.robinson@starz.com>
2019-08-19 14:26:03 -0600

    Refactored WeatherViewModel logic out of MainActivity

--- app/src/main/java/com/twitter/challenge/MainActivity.java
+++ app/src/main/java/com/twitter/challenge/MainActivity.java
@@ -1,7 +1,6 @@
 package com.twitter.challenge;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,47 +23,44 @@ public class MainActivity extends AppCompatActivity {
         final TextView deviationView = findViewById(R.id.deviation);
 
         final WeatherViewModel model = ViewModelProviders.of(this).get(WeatherViewModel.class);
+        model.loadCurrentWeatherConditions();
+        model.loadFutureWeatherConditions();
 
-        Observer<WeatherConditions> currentWeatherObserver = new Observer<WeatherConditions>() {
+        Observer<Float> currentTempObserver = new Observer<Float>() {
             @Override
-            public void onChanged(WeatherConditions weatherConditions) {
-                windSpeedView.setText(String.format("%f", weatherConditions.getWind().getSpeed()));
-                Log.d(TAG, "Cloudiness: " + weatherConditions.getClouds().getCloudiness());
-                if(weatherConditions.getClouds().getCloudiness() > 50) { // TODO Move this and log to ViewModel
+            public void onChanged(Float currentTemp) {
+                temperatureView.setText(getString(R.string.temperature, currentTemp, TemperatureConverter.celsiusToFahrenheit(currentTemp)));
+            }
+        };
+
+        Observer<Double> currentWindSpeedObserver = new Observer<Double>() {
+            @Override
+            public void onChanged(Double currentWindSpeed) {
+                windSpeedView.setText(String.format("%f", currentWindSpeed));
+            }
+        };
+
+        Observer<Boolean> currentCloudinessObserver = new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isCloudy) {
+                if (isCloudy)
                     cloudinessView.setBackground(getResources().getDrawable(R.mipmap.ic_launcher));
-                }
-                temperatureView.setText(getString(R.string.temperature, weatherConditions.getWeather().getTemp(), TemperatureConverter.celsiusToFahrenheit( (float)weatherConditions.getWeather().getTemp())));
-                // TODO hide getTemp() behind ViewModel
             }
         };
-        model.getLiveCurrentWeather().observe(this, currentWeatherObserver);
 
-        Observer<Double> tempDeviationObserver = new Observer<Double>() {
+        Observer<Float> tempDeviationObserver = new Observer<Float>() {
             @Override
-            public void onChanged(Double deviation) {
+            public void onChanged(Float deviation) {
                 // TODO Add a button to fetch this instead of loading it onCreate
-                deviationView.setText(deviation+"");
+                deviationView.setText(deviation + "");
             }
         };
-        model.getLiveFiveDayTempDeviation().observe(this, tempDeviationObserver);
+
+        model.getCurrentTemp().observe(this, currentTempObserver);
+        model.getCurrentWindSpeed().observe(this, currentWindSpeedObserver);
+        model.isCloudy().observe(this, currentCloudinessObserver);
+        model.getFiveDayTempDeviation().observe(this, tempDeviationObserver);
 
     }
 
 }
-//    Coding Exercise for Twitter
-//        Please take no more than 3 hours to complete the following:
-//        Suppose we need a simple Android app to display the weather:
-//        1. Use the provided starter code to get started. 2. Use the API to fetch the current weather. 3. Display the following current conditions:
-//        a. Temperature in Celsius and Fahrenheit b. com.twitter.challenge.Wind speed. c. A cloud icon if the cloudiness percentage is greater than 50%. 4. Provide a button, that when tapped, fetches the weather for the next 5 days, and displays
-//        the standard deviation of the temperature, whose formula is provided below: s tddev = √ i=1 ∑nn−1 (x i
-//        −x) 2
-//        n = number of data points x = average/mean of the data xi = each of the values of the data ∑ is the summation operator which is defined as the addition of all the elements of the series. In this case is the addition of (x i − x ) 2 afor each temperature in the series Because this is a brief exercise and not a full fledged app, you can make the following assumptions:
-//        ● Persisting data across multiple app launches isn’t required.
-//        ● Refreshing the data while the app is running isn’t required.
-//        Additional instructions:
-//        ● We know making a great-looking UI takes a lot longer than 3 hours. Your UI should be functional and responsive, but is not expected to be attractive. You will be judged on the quality of your code, not the style of your UI.
-//        ● You must be able to handle configuration and orientation changes without hitting the network again.
-//        ● The app should not crash under any circumstance, but robust error handling is not necessary. Logging exceptions is sufficient.
-//        ● Consider what would happen if the user’s network speed is slow.
-//        ● Feel free to use any public third-party libraries
-//        ● Test the core functionality.
--- app/src/main/java/com/twitter/challenge/WeatherViewModel.kt
+++ app/src/main/java/com/twitter/challenge/WeatherViewModel.kt
@@ -12,44 +12,48 @@ class WeatherViewModel : ViewModel() {
 
     private val TAG = this.javaClass.simpleName
 
-    val liveCurrentWeather: MutableLiveData<WeatherConditions> by lazy {
-        MutableLiveData<WeatherConditions>().also { loadCurrentWeatherConditions() }
-    }
+    private val weatherApi by lazy { WeatherApi.create() }
 
-    val liveFiveDayTempDeviation: MutableLiveData<Double> by lazy {
-        MutableLiveData<Double>().also { loadFutureWeatherConditions() }
-    }
+    val currentTemp: MutableLiveData<Float> by lazy { MutableLiveData<Float>() }
+    val currentWindSpeed: MutableLiveData<Double> by lazy { MutableLiveData<Double>() }
+    val isCloudy: MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
 
-    private val weatherApi by lazy {
-        WeatherApi.create()
-    }
+    val fiveDayTempDeviation: MutableLiveData<Float> by lazy { MutableLiveData<Float>() }
 
     private var currentWeatherDisposable: Disposable? = null
     private var futureWeatherDisposable: Disposable? = null
 
-    private fun loadCurrentWeatherConditions() {
+    fun loadCurrentWeatherConditions() {
         currentWeatherDisposable = weatherApi.getCurrentCatalog().subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { conditions ->
-                this.liveCurrentWeather.setValue(conditions)
-                    .also { Log.d(TAG, conditions.toString()) }
+                Log.d(TAG, conditions.toString())
+                this.currentTemp.setValue(conditions.weather.temp.toFloat())
+                this.isCloudy.setValue(conditions.clouds.cloudiness > 50)
+                this.currentWindSpeed.setValue(conditions.wind.speed)
             }
 
     }
 
-    private fun loadFutureWeatherConditions() {
-        var listOfFutureTemps = mutableListOf<Double>()
+    fun loadFutureWeatherConditions() {
+        val listOfFutureTemps = mutableListOf<Double>()
 
         val observables = listOf( // TODO create this list dynamically
             weatherApi.getFutureCatalog(1).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()),
             weatherApi.getFutureCatalog(2).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()),
             weatherApi.getFutureCatalog(3).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()),
             weatherApi.getFutureCatalog(4).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()),
-            weatherApi.getFutureCatalog(5).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()))
+            weatherApi.getFutureCatalog(5).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+        )
 
         futureWeatherDisposable = Observable.merge(observables).subscribe { conditions ->
-            listOfFutureTemps.add(conditions.weather.temp.also{ println("Future Temperature: $it List: $listOfFutureTemps" )})
-                .also { liveFiveDayTempDeviation.setValue(TemperatureStatistics.standardDeviation(listOfFutureTemps).also{ println("5 day Deviation: $it")}) }
+            listOfFutureTemps.add(conditions.weather.temp.also { println("Future Temperature: $it List: $listOfFutureTemps") })
+                .also {
+                    fiveDayTempDeviation.setValue(
+                        TemperatureStatistics.standardDeviation(
+                            listOfFutureTemps
+                        ).toFloat().also { println("5 day Deviation: $it") })
+                }
         }
 
     }
